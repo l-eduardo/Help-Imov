b91e3073c36a894489c793b604f069fdd2c779c3
Luiz Eduardo <eduluiz.bit@hotmail.com>
2024-04-19 17:16:46 -0300

    chat domain model

---
+++ domain/models/chat.py
@@ -0,0 +1,26 @@
+from datetime import date
+from typing import List
+import uuid
+
+from domain.models.usuario import Usuario
+
+
+class Chat:
+    def __init__(self, participants: List[Usuario],
+                 messages: List[tuple[uuid.UUID, str, date]],
+                 id: uuid.UUID = uuid.UUID(int=0)):
+        self._id = id
+        self._participantes = participants
+        self._mensagens = messages
+
+    @property
+    def id(self) -> uuid.UUID:
+        return self._id
+
+    @property
+    def participantes(self) -> List[Usuario]:
+        return self._participantes
+
+    @property
+    def mensagens(self) -> List[tuple[uuid.UUID, str, date]]:
+        return self._mensagens
